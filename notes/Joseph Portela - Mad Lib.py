9f05ed071252fc9cf3f88d0bdfbb97b455ca53b0
Joseph Portela <42385543+Joeap36@users.noreply.github.com>
2018-10-30 09:45:46 -0700

    Notes on functions and loops

--- notes/Joseph Portela - Mad Lib.py
+++ notes/Joseph Portela - Mad Lib.py
@@ -1,6 +1,8 @@
 print("Welcome to Your Very Own Mad Lib, otherwise known as YVOML! Please answer each prompt.")
 print()
+
 # Inputs
+
 Plural_noun_1 = input("Enter a plural noun. ")
 Plural_noun_2 = input("Enter another plural noun. ")
 Number_1 = int(input("Enter a number. "))
@@ -13,7 +15,9 @@ Plural_noun_4 = input("Enter yet another plural noun.")
 Noun_2 = input("Enter another noun.")
 Number_2 = int(input("Enter another number."))
 Plural_noun_5 = input("Enter another plural noun.")
+
 # Story
+
 print("Great! YVOML is now complete, here is the result:")
 print("Welcome to the Cult of %s, we believe that %s are really gods in disguise!" % (Plural_noun_1, Plural_noun_2))
 print("We hold services %d days a week, and each service we hold a holy %s." % (Number_1, Ing_verb_1))
