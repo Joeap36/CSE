05671b729a0bbf4bc8aa6d05ccf6ca34aa231888
Joseph Portela <42385543+Joeap36@users.noreply.github.com>
2018-11-07 09:45:47 -0800

    Almost done with Guess Game

--- notes/Joseph Portela - Guess Game.py
+++ notes/Joseph Portela - Guess Game.py
@@ -1,24 +1,31 @@
 import random
-a = (random.randint(1, 10))  # a = Random number
-
-b = 0  # b = User's guess
-c = 0  # c = Number of turns used
-
-while b != (0, a):
-    print("Incorrect")
-    c += 1
 
 
-while c == 5:
-    print("Game Over")
+def main():
+    generated_number = random.randint(1, 10)
+
+    user_guess = 0
+    turns_used = 0
+    win = False
+
+    while turns_used != 5 and not win:
+        user_guess = int(input("Guess a number from 1 to 10"))
+        if user_guess != 0 and user_guess != generated_number:
+            print("Incorrect")
+            turns_used += 1
+            if generated_number < user_guess:
+                print("Lower")
+            elif generated_number > user_guess > 0:
+                print("Higher")
+            user_guess = 0
+        elif user_guess == generated_number:
+            print("You Win!")
+            win = True
+
+    if turns_used == 5:
+        print("Game Over")
+    next_game = input("Play again?")
+    
 
 
-while 0 < a < b:
-    print("Lower")
-    b = 0
-
-
-while 0 < b < a:
-    print("Higher")
-    b = 0
-
+main()
