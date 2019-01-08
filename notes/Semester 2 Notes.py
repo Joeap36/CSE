f8fd2bad40136eaa2f2ba95da0802e1e6ea5a8d2
Joseph Portela <42385543+Joeap36@users.noreply.github.com>
2019-01-08 09:44:58 -0800

    Notes

---
+++ notes/Semester 2 Notes.py
@@ -0,0 +1,22 @@
+print("Hello World!")
+# Big Chungus 5
+
+cars = 5
+driving = True
+
+print("I have %d cars." % cars)
+print("I have " + str(cars) + " cars.")
+
+age = input("How old are you?")
+print(age + "??? How?!")
+
+list1 = ["red", "orange", "yellow", "green", "blue"]
+list1.append("purple")
+print(list1)
+
+list1.pop(0)
+print(list1)
+
+print(list1[2])
+
+print(len(list1))
