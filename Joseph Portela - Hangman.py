c60e967b5bb9117d722c3093e1b731fb19d25eaa
Joseph Portela <42385543+Joeap36@users.noreply.github.com>
2019-01-22 09:44:58 -0800

    Hangman

--- Joseph Portela - Hangman.py
+++ Joseph Portela - Hangman.py
@@ -1,10 +1,28 @@
 import random
 from hangman_addons import *
 
-words = ["avalanche", "big chungus", "chimera", "diameter", "evergreen", "florist", "garrison", "horoscope", "interior",
-         "jubilee", "kiosk", "liaison", "menace", "negative", "obfuscate", "parabola", "quarrel", "remember",
-         "salamander", "turquoise", "undermine", "variable", "wilderness", "xenophobia", "youthful", "zephyr"]
+word_bank = ["avalanche", "big chungus", "chimera", "diameter", "evergreen", "florist", "garrison", "horoscope",
+             "interior", "jubilee", "kiosk", "liaison", "menace", "negative", "obfuscate", "parabola", "quarrel",
+             "remember", "salamander", "turquoise", "undermine", "variable", "wilderness", "xenophobia", "youthful",
+             "zephyr"]
 
+word = random.choice(word_bank)
+guesses = 8
+letters_guessed = []
+letter_list = list(word)
 
-word = random.choice(words)
-guesses = 6
+while guesses > 0:
+    hidden_word = []
+    for letter in word:
+        if letter in letters_guessed:
+            hidden_word.append(letter)
+        else:
+            hidden_word.append("*")
+    print("".join(hidden_word))
+
+    current_guess = input("Type letter: ")
+    letters_guessed.append(current_guess)
+
+    if current_guess not in word:
+        guesses -= 1
+        print("Sorry, guess again.")
