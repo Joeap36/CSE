de7ce96891213b95ec5d63d9066bd4c2f867a59b
Joseph Portela <42385543+Joeap36@users.noreply.github.com>
2019-01-29 09:44:19 -0800

    Dictionary notes

---
+++ notes/Joseph Portela - Dictionary Notes.py
@@ -0,0 +1,76 @@
+states = {
+    "CA": "California",
+    "FL": "Florida",
+    "AK": "Alaska",
+    "GA": "Georgia"
+}
+
+print(states["CA"])
+print(states["AK"])
+
+nested_dictionary = {
+    "CA": {
+        "NAME": "California",
+        "POPULATION": 39500000
+    },
+    "FL": {
+        "NAME": "Florida",
+        "POPULATION": 21300000
+    },
+    "AK": {
+        "NAME": "Alaska",
+        "POPULATION": 737000
+    },
+    "GA": {
+        "NAME": "Georgia",
+        "POPULATION": 10500000
+    }
+}
+
+print(nested_dictionary["GA"]["POPULATION"])
+print(nested_dictionary["FL"]["NAME"])
+
+georgia = nested_dictionary["GA"]
+print(georgia)
+
+complex_dictionary = {
+    "CA": {
+        "NAME": "California",
+        "POPULATION": 39500000,
+        "CITIES": [
+            "Fresno",
+            "San Francisco",
+            "Los Angeles"
+        ]
+    },
+    "FL": {
+        "NAME": "Florida",
+        "POPULATION": 21300000,
+        "CITIES": [
+            "Miami",
+            "Orlando",
+            "Tampa",
+            "Jacksonville"
+        ]
+    },
+    "AK": {
+        "NAME": "Alaska",
+        "POPULATION": 737000,
+        "CITIES": [
+            "Anchorage",
+            "Fairbanks",
+            "Juneau"
+        ]
+    },
+    "GA": {
+        "NAME": "Georgia",
+        "POPULATION": 10500000,
+        "CITIES": [
+            "Atlanta",
+            "Savannah",
+            "Augusta"
+        ]
+    }
+}
+
+print(complex_dictionary["AK"]["CITIES"][0])
