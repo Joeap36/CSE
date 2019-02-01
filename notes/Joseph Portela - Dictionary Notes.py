2fc105c5e9b5571fcc5c573ffb8ad6fbc58b51b0
Joseph Portela <42385543+Joeap36@users.noreply.github.com>
2019-02-01 10:38:57 -0800

    CYOA

--- notes/Joseph Portela - Dictionary Notes.py
+++ notes/Joseph Portela - Dictionary Notes.py
@@ -90,3 +90,8 @@ for state, info in complex_dictionary.items():
         print(stats)
         print("-" * 20)
     print("=" * 20)
+
+# Other Notes
+states["AR"] = "Arizona?"  # It isn't Arizona
+
+states["AR"] = "Arkansas"  # Fixed it
