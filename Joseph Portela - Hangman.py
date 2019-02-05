ed4f166ab559d173e7fbf350348d7d445dbc7877
Joseph Portela <42385543+Joeap36@users.noreply.github.com>
2019-02-05 09:44:32 -0800

    CYOA

--- Joseph Portela - Hangman.py
+++ Joseph Portela - Hangman.py
@@ -1,5 +1,4 @@
 import random
-from hangman_addons import *
 
 word_bank = ["avalanche", "big chungus", "chimera", "diameter", "evergreen", "florist", "garrison", "horoscope",
              "interior", "jubilee", "kiosk", "liaison", "menace", "negative", "obfuscate", "parabola", "quarrel",
@@ -10,19 +9,24 @@ word = random.choice(word_bank)
 guesses = 8
 letters_guessed = []
 letter_list = list(word)
+win = False
 
-while guesses > 0:
+while guesses > 0 and win is False:
     hidden_word = []
     for letter in word:
         if letter in letters_guessed:
             hidden_word.append(letter)
         else:
             hidden_word.append("*")
+    if "*" not in hidden_word:
+        win = True
     print("".join(hidden_word))
-
     current_guess = input("Type letter: ")
-    letters_guessed.append(current_guess)
+    letters_guessed.append(current_guess.lower())
 
-    if current_guess not in word:
+    if current_guess.lower() not in word:
         guesses -= 1
         print("Sorry, guess again.")
+
+if win is True:
+    print("Congratulations, you win!")
