9e1bb5604e543c31d7791a864ab8072ef4e00b2a
Joseph Portela <42385543+Joeap36@users.noreply.github.com>
2019-02-15 09:43:25 -0800

    Object

--- notes/Joseph Portela - Object.py
+++ notes/Joseph Portela - Object.py
@@ -1,5 +1,20 @@
-class Connor(object):
+class Shaggy(object):
     def __init__(self):
-        self.deviant = False
-        self.lieutenant_awake = False
-        self.
+        self.universe = True
+        self.power_percentage = 1
+        self.god = False
+        self.matt_from_wii_sports = True
+        self.most_powerful_being = False
+
+    def increase_power(self):
+        if self.power_percentage < 100:
+            self.power_percentage += 1
+            print("Shaggy increased his power by 1 percent")
+            print("Shaggy now has %d percent power" % self.power_percentage)
+        elif self.power_percentage == 100:
+            self.power_percentage = 9001
+            print("Shaggy's power level is over 9000")
+        else:
+            print("Shaggy cannot increase his power further")
+
+    def
