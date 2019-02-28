3375b00b8151f4b441ab2cf1353f8b73432ee64b
Joseph Portela <42385543+Joeap36@users.noreply.github.com>
2019-02-28 09:44:52 -0800

    Inheritance

---
+++ notes/Inheritance.py
@@ -0,0 +1,47 @@
+class Vehicle(object):
+    def __init__(self):
+        self.name = name
+
+class Car(Vehicle):
+    def __init__(self, name, milage):
+        super(Car, self).__init__(name)
+        self.engine_status = False
+        self.fuel = 100
+        self.milage = milage
+
+    def start_engine(self):
+        self.engine_status = True
+        print("You turn the key and the car turns on.")
+
+    def move_forward(self):
+        self.fuel -= 1
+        print("The car moves forward")
+
+    def turn_left(self):
+        self.fuel -= 1
+        print("The car turns left")
+
+    def turn_off(self):
+        self.engine_status = False
+        print("You turn off the car")
+
+class Impala(Car):
+    def __init__(self):
+        super(Impala, self).__init__("Impala", 25)
+
+class KeylessCar(Car):
+    def __init__(self, name, milage):
+        super(KeylessCar, self).__init__(name, milage)
+
+    def start_engine(self):
+        self.engine_status = True
+        print("You push the button and the car turns on.")
+
+wiebe_car = KeylessCar("Tesla", 125)
+
+thanos_car = Impala()
+thanos_car.start_engine()
+thanos_car.move_forward()
+thanos_car.turn_left()
+thanos_car.move_forward()
+thanos_car.turn_off()
