a0e44a8656b3cd26d942632b1532e4f8c8b45ac0
Joseph Portela <42385543+Joeap36@users.noreply.github.com>
2019-03-12 09:44:11 -0700

    Item

--- notes/Item.py
+++ notes/Item.py
@@ -5,16 +5,21 @@ class Item(object):
 
 
 class Food(Item):
-    def __init__(self, name):
+    def __init__(self, name, healing):
         super(Food, self).__init__(name)
+        self.healing = healing
 
 
 class Weapon(Item):
-    def __init__(self, name, durability, bow):
+    def __init__(self, name, durability, attack, bow, fire, electric, ice):
         super(Weapon, self).__init__(name)
         self.name = name
         self.durability = durability
+        self.attack = attack
         self.bow = bow
+        self.fire = fire
+        self.electric = electric
+        self.ice = ice
 
 
 class Armor(Item):
