6f3cd6a14628108e0afda20407e18c451999551b
Joseph Portela <42385543+Joeap36@users.noreply.github.com>
2019-03-15 10:39:02 -0700

    Items

--- notes/Item.py
+++ notes/Item.py
@@ -11,15 +11,35 @@ class Food(Item):
 
 
 class Weapon(Item):
-    def __init__(self, name, durability, attack, bow, fire, ice, electric):
+    def __init__(self, name, durability, attack):
         super(Weapon, self).__init__(name)
         self.name = name
         self.durability = durability
         self.attack = attack
-        self.bow = bow
-        self.fire = fire
-        self.ice = ice
-        self.electric = electric
+
+
+class ElecWeapon(Weapon):
+    def __init__(self, name, durability, attack):
+        super(ElecWeapon, self).__init__(name, durability, attack)
+        self.name = name
+        self.durability = durability
+        self.attack = attack
+
+
+class IceWeapon(Weapon):
+    def __init__(self, name, durability, attack):
+        super(IceWeapon, self).__init__(name, durability, attack)
+        self.name = name
+        self.durability = durability
+        self.attack = attack
+
+
+class FireWeapon(Weapon):
+    def __init__(self, name, durability, attack):
+        super(FireWeapon, self).__init__(name, durability, attack)
+        self.name = name
+        self.durability = durability
+        self.attack = attack
 
 
 class Armor(Item):
@@ -52,31 +72,49 @@ class Merchant(Character):
         self.stock = stock
 
 
-master_sword = Weapon("Master Sword", 40, 30, False, False, False, False)
-traveler_sword = Weapon("Traveler's Sword", 20, 5, False, False, False, False)
-soldier_broadsword = Weapon("Soldier's Broadsword", 25, 14, False, False, False, False)
-knight_broadsword = Weapon("Knight's Broadsword", 27, 26, False, False, False, False)
-royal_broadsword = Weapon("Royal Broadsword", 36, 36, False, False, False, False)
-forest_dweller_sword = Weapon("Forest Dweller's Sword", 27, 22, False, False, False, False)
-zora_sword = Weapon("Zora Sword", 27, 15, False, False, False, False)
-feathered_edge = Weapon("Feathered Edge", 27, 15, False, False, False, False)
-gerudo_scimitar = Weapon("Gerudo Scimitar", 23, 16, False, False, False, False)
-moonlight_scimitar = Weapon("Moonlight Scimitar", 32, 25, False, False, False, False)
-scimitar_of_the_seven = Weapon("Scimitar of the Seven", 60, 32, False, False, False, False)
-eightfold_blade = Weapon("Eightfold Blade", 26, 15, False, False, False, False)
-ancient_short_sword = Weapon("Ancient Short Sword", 54, 40, False, False, False, False)
-rusty_broadsword = Weapon("Rusty Broadsword", 8, 6, False, False, False, False)
-royal_guard_sword = Weapon("Royal Guard's Sword", 14, 48, False, False, False, False)
-flameblade = Weapon("Flameblade", 36, 24, False, True, False, False)
-frostblade = Weapon("Frostblade", 30, 20, False, False, True, False)
-thunderblade = Weapon("Thunderblade", 36, 22, False, False, False, True)
-goddess_sword = Weapon("Goddess Sword", 45, 28, False, False, False, False)
-boko_club = Weapon("Boko Club", 8, 4, False, False, False, False)
-spiked_boko_club = Weapon("Spiked Boko Club", 14, 12, False, False, False, False)
-dragonbone_boko_club = Weapon("Dragonbone Boko Club", 18, 24, False, False, False, False)
-lizal_boomerang = Weapon("Lizal Boomerang", 17, 14, False, False, False, False)
-lizal_forked_boomerang = Weapon("Lizal Forked Boomerang", 25, 24, False, False, False, False)
-lizal_triboomerang = Weapon("Lizal Tri-Boomerang", 27, 36, False, False, False, False)
-guardian_sword = Weapon("Guardian Sword", 17, 20, False, False, False, False)
-guardian_sword_plus = Weapon("Guardian Sword+", 20, 5, False, False, False, False)
-guardian_sword_plus_plus = Weapon("Guardian Sword++", 20, 5, False, False, False, False)
+# Weapons
+master_sword = Weapon("Master Sword", 40, 30)
+traveler_sword = Weapon("Traveler's Sword", 20, 5)
+soldier_broadsword = Weapon("Soldier's Broadsword", 25, 14)
+knight_broadsword = Weapon("Knight's Broadsword", 27, 26)
+royal_broadsword = Weapon("Royal Broadsword", 36, 36)
+forest_dweller_sword = Weapon("Forest Dweller's Sword", 27, 22)
+zora_sword = Weapon("Zora Sword", 27, 15)
+feathered_edge = Weapon("Feathered Edge", 27, 15)
+gerudo_scimitar = Weapon("Gerudo Scimitar", 23, 16)
+moonlight_scimitar = Weapon("Moonlight Scimitar", 32, 25)
+scimitar_of_the_seven = Weapon("Scimitar of the Seven", 60, 32)
+eightfold_blade = Weapon("Eightfold Blade", 26, 15)
+ancient_short_sword = Weapon("Ancient Short Sword", 54, 40)
+rusty_broadsword = Weapon("Rusty Broadsword", 8, 6)
+royal_guard_sword = Weapon("Royal Guard's Sword", 14, 48)
+flameblade = Weapon("Flameblade", 36, 24)
+goddess_sword = Weapon("Goddess Sword", 45, 28)
+boko_club = Weapon("Boko Club", 8, 4)
+spiked_boko_club = Weapon("Spiked Boko Club", 14, 12)
+dragonbone_boko_club = Weapon("Dragonbone Boko Club", 18, 24)
+lizal_boomerang = Weapon("Lizal Boomerang", 17, 14)
+lizal_forked_boomerang = Weapon("Lizal Forked Boomerang", 25, 24)
+lizal_triboomerang = Weapon("Lizal Tri-Boomerang", 27, 36)
+guardian_sword = Weapon("Guardian Sword", 17, 20)
+guardian_sword_plus = Weapon("Guardian Sword+", 20, 5)
+guardian_sword_plus_plus = Weapon("Guardian Sword++", 20, 5)
+lynel_sword = Weapon("Lynel Sword", 26, 24)
+mighty_lynel_sword = Weapon("Mighty Lynel Sword", 32, 36)
+savage_lynel_sword = Weapon("Savage Lynel Sword", 41, 58)
+fire_rod = Weapon("Fire Rod", 14, 5)
+meteor_rod = Weapon("Meteor Rod", 32, 10)
+vicious_sickle = Weapon("Vicious Sickle", 14, 16)
+demon_carver = Weapon("Demon Carver", 25, 40)
+
+# Electric Weapons
+thunderblade = ElecWeapon("Thunderblade", 36, 22)
+lightning_rod = ElecWeapon("Lightning Rod", 14, 5)
+thunderstorm_rod = ElecWeapon("Thunderstorm Rod", 32, 10)
+
+# Ice Weapons
+frostblade = IceWeapon("Frostblade", 30, 20)
+ice_rod = Weapon("Ice Rod", 14, 5)
+blizzard_rod = Weapon("Blizzard Rod", 32, 10)
+
+# Fire Weapons
