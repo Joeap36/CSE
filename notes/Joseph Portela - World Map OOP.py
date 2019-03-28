4c79400e8c472ad939e3e89a2e9756a5dda60c57
Joseph Portela <42385543+Joeap36@users.noreply.github.com>
2019-03-28 09:43:32 -0700

    CYOA

--- notes/Joseph Portela - World Map OOP.py
+++ notes/Joseph Portela - World Map OOP.py
@@ -1,5 +1,5 @@
 class Room(object):
-    def __init__(self, name, north=None, east=None, south=None, west=None, worp=None, description=None):
+    def __init__(self, name, north=None, east=None, south=None, west=None, worp=None, description=None, items=None):
         self.name = name
         self.north = north
         self.east = east
@@ -7,12 +7,13 @@ class Room(object):
         self.west = west
         self.worp = worp
         self.description = description
+        self.items = items
 
 
 class Player(object):
-    def __init__(self, starting_location):
+    def __init__(self, starting_location, inventory):
         self.current_location = starting_location
-        self.inventory = []
+        self.inventory = inventory
 
     def move(self, new_location):
         """This moves the player to a new room
@@ -31,37 +32,161 @@ class Player(object):
 
 
 vworp = Room("Vworp", None, None, None, None, None, "Vworp")
-white = Room("The White Room", None, None, None, None, None)
-yellow = Room("The Yellow Room", None, None, white, None, None)
-scarlet = Room("The Scarlet Room", None, None, None, white, None)
-violet = Room("The Violet Room", white, None, None, None, None)
-turquoise = Room("The Turquoise Room", None, white, None, None, None)
-red = Room("The Red Room", None, None, scarlet, yellow, None)
-purple = Room("The Purple Room", scarlet, None, None, violet, None)
-cyan_blue = Room("The Cyan Blue Room", turquoise, violet, None, None, None)
-yellow_green = Room("The Yellow Green Room", None, yellow, turquoise, None, None)
-orange_yellow = Room("The Orange Yellow Room", vworp, None, yellow, None, None)
-orange = Room("The Orange Room", None, None, red, orange_yellow, None)
-orange_red = Room("The Orange Red Room", None, None, None, orange, None)
-scarlet_red = Room("The Scarlet Red Room", orange_red, None, None, red, None)
-crimson = Room("The Crimson Room", scarlet_red, vworp, None, scarlet, None)
-magenta = Room("The Magenta Room", crimson, None, None, purple, None)
-purple_magenta = Room("The Purple Magenta Room", magenta, None, None, None, None)
-purple_violet = Room("The Purple Violet Room", purple, purple_magenta, None, None, None)
-blue_violet = Room("The Blue Violet Room", violet, purple_violet, vworp, None, None)
-blue = Room("The Blue Room", cyan_blue, blue_violet, None, None, None)
-blueish_cyan = Room("The Blueish Cyan Room", None, blue, None, None, None)
-greenish_cyan = Room("The Greenish Cyan Room", None, cyan_blue, blueish_cyan, None, None)
-blueish_green = Room("The Blueish Green Room", None, turquoise, greenish_cyan, vworp, None)
-green = Room("The Green Room", None, yellow_green, blueish_green, None, None)
-sap_green = Room("The Sap Green Room", None, None, green, None, None)
-lemon_yellow = Room("The Lemon Yellow Room", None, orange_yellow, yellow_green, sap_green, None)
+white = Room("The White Room", None, None, None, None, None,
+             "The room is almost blindingly white, there"
+             "are four passages, each on one side of the"
+             "room. Violet is to the south, scarlet is to"
+             "the east, yellow is to the north, and"
+             "turquoise is to the west."
+             )
+yellow = Room("The Yellow Room", None, None, white, None, None,
+              "The room is completely yellow. Four exits, one"
+              "on each wall, leave the room. Orange yellow is"
+              "to the north, red is to the east, white is to"
+              "the south, and yellow green is to the west."
+              )
+scarlet = Room("The Scarlet Room", None, None, None, white, None,
+               "The room is completely scarlet. Four exits,"
+               "one on each wall, leave the room. red is to"
+               "the north, crimson is to the east, purple is"
+               "to the south, and white is to the west."
+               )
+violet = Room("The Violet Room", white, None, None, None, None,
+              "The room is completely violet. Four exits, one"
+              "on each wall, leave the room. White is to the"
+              "north, purple is to the east, blue violet is to"
+              "the south, and cyan blue is to the west."
+              )
+turquoise = Room("The Turquoise Room", None, white, None, None, None,
+                 "The room is completely turquoise. Four exits, one"
+                 "on each wall, leave the room. Yellow green is to"
+                 "the north, white is to the east, cyan blue is to"
+                 "the south, and blueish green is to the west."
+                 )
+red = Room("The Red Room", None, None, scarlet, yellow, None,
+           "The room is completely red. Four exits, one"
+           "on each wall, leave the room. Orange is to the"
+           "north, scarlet red is to the east, scarlet is"
+           "to the south, and yellow is to the west."
+           )
+purple = Room("The Purple Room", scarlet, None, None, violet, None,
+              "The room is completely purple. Four exits, one"
+              "on each wall, leave the room. Scarlet is to the"
+              "north, magenta is to the east, purple violet is"
+              "to the south, and violet is to the west."
+              )
+cyan_blue = Room("The Cyan Blue Room", turquoise, violet, None, None, None,
+                 "The room is completely cyan blue. Four exits,"
+                 "one on each wall, leave the room. Turquoise"
+                 "is to the north, violet is to the east, blue"
+                 "is to the south, and greenish cyan is to the"
+                 "west."
+                 )
+yellow_green = Room("The Yellow Green Room", None, yellow, turquoise, None, None,
+                    "The room is completely yellow green. Four exits,"
+                    "one on each wall, leave the room. Lemon yellow is"
+                    "to the north, yellow is to the east, turquoise is"
+                    "to the south, and green is to the west."
+                    )
+orange_yellow = Room("The Orange Yellow Room", vworp, None, yellow, None, None,
+                     "The room is orange yellow. Four exits, one"
+                     "on each wall, leave the room. A strange,"
+                     "portal-like door is to the north, orange is"
+                     "to the east, yellow is to the south, and"
+                     "lemon yellow is to the west."
+                     )
+orange = Room("The Orange Room", None, None, red, orange_yellow, None,
+              "The room is completely orange. Three exits"
+              "leave the room. Orange red is to the east, red is"
+              "to the south, and orange yellow is to the west."
+              )
+orange_red = Room("The Orange Red Room", None, None, None, orange, None,
+                  "The room is completely orange red. Two exits"
+                  "leave the room. Scarlet red is to the south"
+                  "and orange is to the west."
+                  )
+scarlet_red = Room("The Scarlet Red Room", orange_red, None, None, red, None,
+                   "The room is completely scarlet red. Three exits"
+                   "leave the room. Orange red is to the north, crimson"
+                   "is to the south, and red is to the west."
+                   )
+crimson = Room("The Crimson Room", scarlet_red, vworp, None, scarlet, None,
+               "The room is completely crimson. Four exits, one"
+               "on each wall, leave the room. Scarlet red is"
+               "to the north, a strange, portal-like door is"
+               "to the east, magenta is to the south, and"
+               "scarlet is to the west."
+               )
+magenta = Room("The Magenta Room", crimson, None, None, purple, None,
+               "The room is completely magenta. Three exits"
+               "leave the room. Crimson is to the north, purple"
+               "magenta is to the south, and purple is to the"
+               "west."
+               )
+purple_magenta = Room("The Purple Magenta Room", magenta, None, None, None, None,
+                      "The room is completely violet. Two exits"
+                      "leave the room. Magenta is to the north"
+                      "and purple violet is to the west."
+                      )
+purple_violet = Room("The Purple Violet Room", purple, purple_magenta, None, None, None,
+                     "The room is completely purple violet. Three exits"
+                     "leave the room. Purple is to the north, purple"
+                     "magenta is to the east, and blue violet is to the"
+                     "west."
+                     )
+blue_violet = Room("The Blue Violet Room", violet, purple_violet, vworp, None, None,
+                   "The room is completely blue violet. Four exits,"
+                   "one on each wall, leave the room. Violet is to"
+                   "the north, purple violet is to the east, a strange,"
+                   " portal-like door is to the south, and blue is to"
+                   "the west."
+                   )
+blue = Room("The Blue Room", cyan_blue, blue_violet, None, None, None,
+            "The room is completely blue. Three exits leave the"
+            "room. Cyan Blue is to the north, blue violet is to"
+            "the east, and blueish cyan is to the west."
+            )
+blueish_cyan = Room("The Blueish Cyan Room", None, blue, None, None, None,
+                    "The room is completely blueish cyan. Two"
+                    "exits leave the room. Greenish cyan is to"
+                    "the north, and blue is to the east."
+                    )
+greenish_cyan = Room("The Greenish Cyan Room", None, cyan_blue, blueish_cyan, None, None,
+                     "The room is completely greenish cyan. Three exits"
+                     "leave the room. Blueish green is to the north, cyan"
+                     "blue is to the east, and blueish cyan is to the"
+                     "south."
+                     )
+blueish_green = Room("The Blueish Green Room", None, turquoise, greenish_cyan, vworp, None,
+                     "The room is completely blueish green. Four exits,"
+                     "one on each wall, leave the room. Green is to the"
+                     "north, turquoise is to the east, greenish cyan is"
+                     "to the south, and a strange, portal-like door is to"
+                     "the west."
+                     )
+green = Room("The Green Room", None, yellow_green, blueish_green, None, None,
+             "The room is completely green. Three exits leave"
+             "the room. Sap green is to the north, yellow"
+             "green is to the east, and blueish green is to the"
+             "south."
+             )
+sap_green = Room("The Sap Green Room", None, None, green, None, None,
+                 "The room is completely sap green. Two exits"
+                 "leave the room. Lemon yellow is to the east, and"
+                 "green is to the south."
+                 )
+lemon_yellow = Room("The Lemon Yellow Room", None, orange_yellow, yellow_green, sap_green, None,
+                    "The room is completely lemon yellow. Three exits"
+                    "leave the room. Orange yellow is to the east,"
+                    "yellow green is to the south, and sap green is to"
+                    "the west."
+                    )
 
 vworp.vworp = white
 white.north = yellow
-white.west = scarlet
+white.east = scarlet
 white.south = violet
-white.east = turquoise
+white.west = turquoise
 yellow.north = orange_yellow
 yellow.east = red
 yellow.west = yellow_green
@@ -301,7 +426,7 @@ red_bokoblin = Enemy("Red Bokoblin", 1, 13)
 beetle = Merchant("Beetle", [boko_club, wooden_bow])
 
 # Controls
-player = Player(white)
+player = Player(white, [])
 
 playing = True
 directions = ['north', 'south', 'east', 'west', 'vworp']
