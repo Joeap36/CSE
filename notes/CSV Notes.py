d67309138bceb3dce15b8c73b4f2b9f6197e18bc
Joseph Portela <42385543+Joeap36@users.noreply.github.com>
2019-04-26 09:48:19 -0700

    CSV

--- notes/CSV Notes.py
+++ notes/CSV Notes.py
@@ -1,5 +1,11 @@
 import csv
 
+
+def validate(num: str):
+    if len(str(old_number)) == 16:
+        return True
+    return False
+
 # with open("Book1.csv") as old_csv:
 #    with open("MyNewFile.csv", 'w', newline='') as new_csv:
 #        print("Writing file...   ")
@@ -14,14 +20,27 @@ import csv
 #             print(old_number)
 # print("OK")
 
+# with open("Book1.csv") as old_csv:
+#     with open("MyNewFile.csv", 'w', newline='') as new_csv:
+#         print("Writing file...   ")
+#         reader = csv.reader(old_csv)
+#         writer = csv.writer(new_csv)
+#         for row in reader:
+#             old_number = row[0]  # This is a string
+#             first_num = int(old_number[0])  # This is the first
+#             if first_num % 2 == 0:
+#                 writer.writerow(row)
+# print("OK")
+
+
 with open("Book1.csv") as old_csv:
     with open("MyNewFile.csv", 'w', newline='') as new_csv:
         print("Writing file...   ")
         reader = csv.reader(old_csv)
         writer = csv.writer(new_csv)
+
         for row in reader:
             old_number = row[0]  # This is a string
-            first_num = int(old_number[0])  # This is the first
-            if first_num % 2 == 0:
+            if validate(old_number):
                 writer.writerow(row)
-print("OK")
+    print("OK")
