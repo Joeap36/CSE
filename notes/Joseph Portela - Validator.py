1c327f53230f5f34a426b9b51d26ea5a8cdb619c
Joseph Portela <42385543+Joeap36@users.noreply.github.com>
2019-05-02 09:37:17 -0700

    Validator

--- notes/Joseph Portela - Validator.py
+++ notes/Joseph Portela - Validator.py
@@ -14,28 +14,29 @@ def reverse(num: str):
 def validate(num: str):
     if not check_len(num):
         return False
-    last_digit = num[15]
-    rest_of_nums = num[:15]
-    reversed = reverse(rest_of_nums)
-    for index in range(len(reversed)):
-        reversed[index] = int(reversed[index])
-        if index % 2 == 0:
-            index *= 2
-            if index > 9:
-                index -= 9
+    else:
+        last_digit = num[15]
+        rest_of_nums = num[:15]
+        reversed_num = reverse(rest_of_nums)
+        for index in range(len(reversed_num)):
+            reversed_num[index] = int(reversed_num[index])
+            if index % 2 == 0:
+                reversed_num[index] *= 2
+                if reversed_num[index] > 9:
+                    reversed_num[index] -= 9
+        total = sum(reversed_num)
+        if total % 10 != int(last_digit):
+            return False
+        else:
+            return True
 
-    print(reversed)
 
-
-# print(len("7009235029262260"))
-print(validate("7009235029262260"))
-
-# with open("Book1.csv") as old_csv:
-#     with open("MyNewFile.csv", 'w', newline='') as new_csv:
-#         print("Writing file...   ")
-#         reader = csv.reader(old_csv)
-#         writer = csv.writer(new_csv)
-#         for row in reader:
-#             old_number = row[0]  # This is a string
-#             if validate(old_number):
-#                 writer.writerow(row)
+with open("Book1.csv") as old_csv:
+    with open("MyNewFile.csv", 'w', newline='') as new_csv:
+        print("Writing file...   ")
+        reader = csv.reader(old_csv)
+        writer = csv.writer(new_csv)
+        for row in reader:
+            old_number = row[0]
+            if validate(old_number):
+                writer.writerow(row)
