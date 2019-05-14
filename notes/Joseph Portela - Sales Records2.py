0a3663adafc7f1e7b04bf9136de8909dfd06e7af
Joseph Portela <42385543+Joeap36@users.noreply.github.com>
2019-05-14 09:43:01 -0700

    Sales

--- notes/Joseph Portela - Sales Records2.py
+++ notes/Joseph Portela - Sales Records2.py
@@ -4,9 +4,8 @@ items = {}
 
 with open("Sales Records.csv") as old_csv:
     reader = csv.reader(old_csv)
+    best_profit = 0
     best_item = None
-    country_profit = 0
-    best_country = None
     for row in reader:
         if row[0] == 'Region':
             continue
@@ -19,9 +18,19 @@ with open("Sales Records.csv") as old_csv:
         except KeyError:
             items[item_type] = {}
             items[item_type]['Total Profit'] = item_profit
-        if items[item_type]['Best Country'] < item_profit:
-            try:
+        try:
+            if items[item_type]['Country Profit'] < item_profit:
                 items[item_type]['Best Country'] = country
-            except KeyError:
-                
-print(items)
+                items[item_type]['Country Profit'] = item_profit
+        except KeyError:
+            items[item_type]['Country Profit'] = item_profit
+            items[item_type]['Best Country'] = country
+for product in items.keys():
+    print("The total profit from %s is %d, and the best country to advertise this to is %s, with a profit of %d."
+          % (product, items[product]['Total Profit'], items[product]['Best Country'],
+             items[product]['Country Profit']))
+    if items[product]['Total Profit'] > best_profit:
+        best_profit = items[product]['Total Profit']
+        best_item = product
+print()
+print("The best product for the company is %s, with a profit of %d." % (best_item, best_profit))
